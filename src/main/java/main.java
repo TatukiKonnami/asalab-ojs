959cba9b32d6ae8498484514f92a6a34ee73601f
TatukiKonnami <dmb1703@stumail.daido-it.ac.jp>
2018-03-01 03:14:54 +0900

    judge method create

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,22 +1,15 @@
 
 import analaysis.AnalysisInternalCode;
 import codeCompile.ComplieCode;
+import judge.Judge;
 
 import java.io.IOException;
 
 class Main {
     public static void main(String[] args){
-        AnalysisInternalCode Code = new AnalysisInternalCode();
-        try {
-            ComplieCode.chageInterCode(args[0]);
-            ComplieCode.dumpAST(args[0]);
-            Code.loadInterCode(args[0]+".ll");
-            Code.loadASTFile(args[0]);
-            Code.checkCategory();
-            Code.getCategories().forEach(s -> System.out.println(s.getKind() + " : " + s.getScore()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        Judge judge = new Judge();
+        ComplieCode.chageInterCode(args[0]);
+        ComplieCode.dumpAST(args[0]);
+        judge.judgeCodeElement(args[0]);
     }
 }
